6fd201e395e59c372d1a3f158e6ea5d53458726f
Eva S. Poulsen <eva@btech.au.dk>
2022-04-21 14:56:58 +0200

    Todolist

--- src/components/Todolist.js
+++ src/components/Todolist.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View , Text, StyleSheet } from "react-native";
 
 class TodoList extends Component {
     render() {

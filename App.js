ffd7b3af71d4d4f783ce3d42b1018890d024094e
Eva S. Poulsen <eva@btech.au.dk>
2022-04-21 18:59:23 +0200

    Beautify

--- App.js
+++ App.js
@@ -13,7 +13,7 @@ const App = () => {
           <Navbar />
           <TodoList />
         </ThemeContextProvider>
-        </AuthContextProvider>
+      </AuthContextProvider>
     </View>
   );
 }

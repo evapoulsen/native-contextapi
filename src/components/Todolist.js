eaafdaecd1151d8dcea601f52908a3aa6055d3ea
Eva S. Poulsen <eva@btech.au.dk>
2022-04-21 16:37:50 +0200

    Todolist Context.Consumer

--- src/components/Todolist.js
+++ src/components/Todolist.js
@@ -4,23 +4,26 @@ import { View , Text, StyleSheet } from "react-native";
 import { ThemeContext } from "../contexts/ThemeContext";
 
 class TodoList extends Component {
-    static contextType = ThemeContext;
 
     render() {
-        const { isDarkTheme, darkTheme, lightTheme } = this.context;
-
-        const theme = isDarkTheme ? darkTheme : lightTheme;
-
         return (
-            <View style={[styles.todoContainer, theme]}>
-                <Text style={[styles.item, theme]}>Define scope for the project</Text>
-                <Text style={[styles.item, theme]}>Gather data and content</Text>
-                <Text style={[styles.item, theme]}>Prepare design templates</Text>
-                <Text style={[styles.item, theme]}>Go fo a walk and get some fresh air</Text>
-                <Text style={[styles.item, theme]}>Perform design tests</Text>
-                <Text style={[styles.item, theme]}>Add functionality</Text>
-            </View>
-        )
+            <ThemeContext.Consumer>
+                {(context) => {
+                    const { isDarkTheme, darkTheme, lightTheme } = context;
+                    const theme = isDarkTheme ? darkTheme : lightTheme;
+                    return (
+                        <View style={[styles.todoContainer, theme]}>
+                            <Text style={[styles.item, theme]}>Define scope for the project</Text>
+                            <Text style={[styles.item, theme]}>Gather data and content</Text>
+                            <Text style={[styles.item, theme]}>Prepare design templates</Text>
+                            <Text style={[styles.item, theme]}>Go fo a walk and get some fresh air</Text>
+                            <Text style={[styles.item, theme]}>Perform design tests</Text>
+                            <Text style={[styles.item, theme]}>Add functionality</Text>
+                        </View>
+                    );
+                }}
+            </ThemeContext.Consumer>
+        );
     }
 }
 

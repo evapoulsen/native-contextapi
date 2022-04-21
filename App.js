e7c656b57fa3334dc8aca892e8ff8ff76ba0dd66
Eva S. Poulsen <eva@btech.au.dk>
2022-04-21 18:42:31 +0200

    AuthContext

--- App.js
+++ App.js
@@ -3,13 +3,16 @@ import { StyleSheet, View, Text } from "react-native";
 import Navbar from "./src/components/Navbar";
 import TodoList from "./src/components/Todolist";
 import ThemeContextProvider from "./src/contexts/ThemeContext";
+import AuthContextProvider from "./src/contexts/AuthContext";
 
 const App = () => {
   return (
     <View>
       <ThemeContextProvider>
-        <Navbar />
-        <TodoList />
+        <AuthContextProvider>
+          <Navbar />
+          <TodoList />
+        </AuthContextProvider>
       </ThemeContextProvider>
     </View>
   );
